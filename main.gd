0204f01d538a5c3a02a7f93612ae465c10fd9045
JMO1212 <51058258+JMO1212@users.noreply.github.com>
2025-03-10 09:44:34 -0500

    added main menu and shuffled questions

---
+++ main.gd
@@ -0,0 +1,14 @@
+extends Control
+
+
+
+func _on_chapter_1011_checkpoint_pressed() -> void:
+	get_tree().change_scene_to_file("res://Chapters/chapter_10_11_checkpoint.tscn")
+
+
+func _on_chapter_11_exam_pressed() -> void:
+	get_tree().change_scene_to_file("res://Chapters/chapter_11_exam.tscn")
+
+
+func _on_chapter_11_match_questions_pressed() -> void:
+	get_tree().change_scene_to_file("res://Chapters/chapter_11_match_questions.tscn")
